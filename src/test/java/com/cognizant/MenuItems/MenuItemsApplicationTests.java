f5812dde15464edd3da3c5bc5449e66972f87b47
nipunm1 <nipunmiglani.96@gmail.com>
2022-06-28 16:31:49 +0530

    Initial commit

---
+++ src/test/java/com/cognizant/MenuItems/MenuItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.MenuItems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
